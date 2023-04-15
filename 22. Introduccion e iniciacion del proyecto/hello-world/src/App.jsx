7f1167ab02d2dc9e6a4e0af55d902d2b62a923b4
percy chuzon <percyrondan1995@gmail.com>
2023-04-15 18:28:59 -0500

    Introduccion e iniciacion del proyecto

---
+++ 22. Introduccion e iniciacion del proyecto/hello-world/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+//components
+import Header from "./components/header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ 22. Introduccion e iniciacion del proyecto/hello-world/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Soy el Header</h1>
+      <h1>Subtitulo del header</h1>
+    </>
+  );
+};
+
+export default Header;
