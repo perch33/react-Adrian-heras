d89f9e14467d4576a9cad91a666ef0bd310143f3
percy chuzon <percyrondan1995@gmail.com>
2023-04-18 20:27:48 -0500

    practica de repaso

---
+++ 28. Practica de repaso/hello-world/src/components/person.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Person = ({ info, hobbies, bio, render, children }) => {
+  console.log(children);
+  if (render) {
+    return (
+      <>
+        <p>
+          Soy {info.name} y tengo {info.age} años, Nací en
+          {info.birthday.getFullYear()}, Mis hobbies son {hobbies.join(" ")}
+        </p>
+        {bio}
+      </>
+    );
+  } else {
+    return <p>No hay persona</p>;
+  }
+};
+
+export default Person;
