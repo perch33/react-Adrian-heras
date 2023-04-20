9a190147df1f241c1f022c95dc9a376a3db79061
percy chuzon <percyrondan1995@gmail.com>
2023-04-19 20:17:51 -0500

    Practica corregida

---
+++ 29. Practica corregida/hello-world/src/App.jsx
@@ -0,0 +1,75 @@
+import "./App.css";
+import { generate as id } from "shortid";
+//components
+import Card from "./components/Card";
+
+/* */
+
+function App() {
+  const cards = [
+    {
+      id: id(),
+      title: "Card 1",
+      img: "/img/gato.jpg",
+      author: "Perch33",
+      date: new Date().toLocaleDateString(),
+      tags: ["animal", "gato", "michi"],
+      views: 23,
+      description:
+        "hola com estas este es un cuerpo de relleno de prueba para la card",
+    },
+    {
+      id: id(),
+      title: "Card 2",
+      img: "./img/gato.jpg",
+      author: "Perch33",
+      date: new Date().toLocaleDateString(),
+      tags: ["animal", "gato", "michi"],
+      views: 253,
+      description:
+        "hola com estas este es un cuerpo de relleno de prueba para la card",
+    },
+    {
+      id: id(),
+      title: "Card 3",
+      img: "../img/gato.jpg",
+      author: "Perch33",
+      date: new Date().toLocaleDateString(),
+      tags: ["animal", "gato", "michi"],
+      views: 123,
+      description:
+        "hola com estas este es un cuerpo de relleno de prueba para la card",
+    },
+    {
+      id: id(),
+      title: "Card 4",
+      img: "/img/gato.jpg",
+      author: "Perch33",
+      date: new Date().toLocaleDateString(),
+      tags: ["animal", "gato", "michi"],
+      views: 523,
+      description:
+        "hola com estas este es un cuerpo de relleno de prueba para la card",
+    },
+  ];
+
+  return (
+    <div className="cards">
+      {cards.map((card) => (
+        <Card
+          key={card.id}
+          author={card.author}
+          date={card.date}
+          img={card.img}
+          title={card.title}
+          tags={card.tags}
+          views={card.views}
+        >
+          {card.description}
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ 29. Practica corregida/hello-world/src/components/Card.jsx
@@ -0,0 +1,25 @@
+/* imr para import react */
+import React from "react";
+import { generate as id } from "shortid";
+
+const Card = ({ author, date, img, title, tags, views, children }) => {
+  return (
+    <div className="card">
+      <h2 className="card__title">{title}</h2>
+      <img className="card__img" src={img} alt={title} />
+      <p className="card__description">{children}</p>
+      <p className="card__author">{author}</p>
+      <ul className="card__tags">
+        {tags.map((tag) => (
+          <li key={id()} className="card__tag">
+            {tag}
+          </li>
+        ))}
+      </ul>
+      <p className="card__views">{views}</p>
+      <p className="card__date">{date}</p>
+    </div>
+  );
+};
+
+export default Card;
