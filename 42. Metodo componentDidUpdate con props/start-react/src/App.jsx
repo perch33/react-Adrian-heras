e879a672b625b620bc79b28e2b50d995196bbd36
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 17:44:17 -0500

    Metodo componentDidUpdate con props

---
+++ 42. Metodo componentDidUpdate con props/start-react/src/App.jsx
@@ -0,0 +1,72 @@
+// import { useState } from "react";
+import React, { Component, Fragment } from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+/* con el comando rsc crea el componente e importa el react */
+
+class App extends Component {
+  // state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: 0,
+      post: {},
+      message: "Lifecycle",
+    };
+  }
+
+  handleId = () => {
+    this.setState({ id: this.state.id + 1 });
+  };
+  handleMessage = () => {
+    this.setState({ message: "Lifecycle Cambiado" });
+  };
+
+  async componentDidUpdate(prevProp, prevState) {
+    console.log("update");
+
+    if (prevState.id !== this.state.id) {
+      const { id } = this.state;
+      const rest = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      );
+      const data = await rest.json();
+      //Actualización de eventos
+      this.setState({ post: data });
+    }
+  }
+
+  render() {
+    /* hacer un set state genera un bucle infinito en el render*/
+    const { post } = this.state;
+    if (true) {
+      return (
+        <>
+          <Header title={this.state.message} />
+          <div>
+            {/* {posts.map((post) => (
+              <Fragment key={post.id}>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </Fragment>
+            ))} */}
+
+            {
+              <>
+                <button onClick={this.handleId}>Next Id</button>
+                <button onClick={this.handleMessage}>Change Header</button>
+                <h2>Post con id: {this.state.id}</h2>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </>
+            }
+          </div>
+        </>
+      );
+      /* disponible desde la version 16 */
+    } else return null;
+  }
+}
+
+export default App;
