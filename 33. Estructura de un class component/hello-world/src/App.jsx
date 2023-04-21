cb87f589551e51c59871ff04806be09c4a18ab6f
percy chuzon <percyrondan1995@gmail.com>
2023-04-20 21:51:12 -0500

    Estructura de un class component

---
+++ 33. Estructura de un class component/hello-world/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+/* El estado siempre tiene que ser un objeto, poner state == a null es como no poner nada */
+
+class App extends Component {
+  /* constructor(props) {
+    super(props);
+    this.state = { otherNumber: 5 };
+  } */
+  state = { number: 5 };
+
+  render() {
+    const { number, name } = this.props;
+    return (
+      <h1>
+        Class Component {number}, name {name}
+      </h1>
+    );
+  }
+}
+
+export default App;
