373e9c0d4fd29804a8d8635e355b4bfcb0a7e7b1
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 16:20:50 -0500

    Metodo render

--- 39. Metodo render/start-react/src/App.jsx
+++ 39. Metodo render/start-react/src/App.jsx
@@ -1,23 +1,29 @@
 // import { useState } from "react";
 import React, { Component } from "react";
-
 import "./App.css";
+import Header from "./components/Header";
+
+/* con el comando rsc crea el componente e importa el react */
 
 class App extends Component {
   // state = {};
   constructor(props) {
     super(props);
     this.state = { number: 0 };
-    /* bindear eventos */
     console.log("Constructor");
   }
   render() {
-    return (
-      <>
-        <h1>LifeCycle</h1>
-        <h2>Number os state: {this.state.number} </h2>
-      </>
-    );
+    console.log("Render");
+    /* hacer un set state genera un bucle infinito en el render*/
+    if (false) {
+      return (
+        <>
+          <h1>LifeCycle</h1>
+          <h2>Number os state: {this.state.number} </h2>
+        </>
+      );
+      /* disponible desde la version 16 */
+    }else return null
   }
 }
 
