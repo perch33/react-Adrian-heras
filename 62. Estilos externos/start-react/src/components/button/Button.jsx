5dc65564ce11657ed4dcd7b34d3a98f244c6da69
percy chuzon <percyrondan1995@gmail.com>
2023-04-22 18:49:34 -0500

    Estilos externos

---
+++ 62. Estilos externos/start-react/src/components/button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./button.css";
+
+/* para los estilos con css se recomienda usar en una carpeta para el componente  */
+
+const Button = () => {
+  return <button className="button">Click Me!</button>;
+};
+
+export default Button;
