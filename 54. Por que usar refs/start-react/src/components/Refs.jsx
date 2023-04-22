d605a366f1c47727fff8306c91d9c3a349439a78
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 22:03:45 -0500

    ¿Por qué usar refs?

---
+++ 54. Por que usar refs/start-react/src/components/Refs.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Refs extends Component {
+  state = {};
+  handleFocus = () => {
+    document.getElementById("input").focus();
+  };
+  render() {
+    return (
+      <>
+        <input type="text" id="input" />
+        <button onClick={this.handleFocus}>Focus</button>
+      </>
+    );
+  }
+}
+
+export default Refs;
