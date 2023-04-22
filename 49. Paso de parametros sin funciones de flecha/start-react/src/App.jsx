198c0c54f1486525485286cdbe505350eca0114e
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 21:19:53 -0500

    Paso de parametros sin funciones de flecha

---
+++ 49. Paso de parametros sin funciones de flecha/start-react/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    handleClick(msg, msg2, e) {
+        console.log(e)
+        console.log(msg)
+        console.log(msg2)
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Events</h1>
+                <button onClick={this.handleClick.bind(this, 'hello', 'world')}>Click me!</button>
+            </>
+        )
+    }
+}
+
+
+export default App;
