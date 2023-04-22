0e4b12d5dd33df2004bd17303aea1b0f045655b6
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 21:29:36 -0500

    Eventos personalizados

---
+++ 50. Eventos personalizados/start-react/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Button from "./components/Button";
+
+class App extends Component {
+  handleClick = (e) => {
+    console.log(e.target);
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Events</h1>
+        {/* <button onClick={this.handleClick}>Click me!</button> */}
+        <Button text="Click me!" onEventClick={this.handleClick} />
+      </>
+    );
+  }
+}
+
+export default App;
