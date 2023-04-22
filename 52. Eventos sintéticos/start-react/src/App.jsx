32222459002cd1d8c9e05b9fd0ec6010410bdf70
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 21:48:59 -0500

    Eventos sintéticos

---
+++ 52. Eventos sintéticos/start-react/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  handleClick(e) {
+    console.log(e);
+    console.log(e.nativeEvent);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Events</h1>
+        <button onClick={this.handleClick}>Click me!</button>
+      </>
+    );
+  }
+}
+
+export default App;
