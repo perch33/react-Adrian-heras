64a1afbf57212c7194d20236180e39e46ed5f742
percy chuzon <percyrondan1995@gmail.com>
2023-04-21 22:20:45 -0500

    Controlando input con refs

---
+++ 56. Controlando input con refs/start-react/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Form from "./components/Form";
+// import Refs from "./components/Refs";
+
+
+class App extends Component {
+  render() {
+    return(
+      <>
+      {/* <Refs /> */}
+      <Form />
+      </>
+    ) ;
+  }
+}
+
+export default App;
---
+++ 56. Controlando input con refs/start-react/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ text }) => {
+  const handleClick = (e) => {
+    console.log(e.target);
+  };
+
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+export default Button;
