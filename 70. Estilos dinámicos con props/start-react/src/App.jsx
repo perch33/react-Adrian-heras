c2a874fe7bb9f1a20db02cb6544a23b4f2b7ae85
percy chuzon <percyrondan1995@gmail.com>
2023-04-22 21:44:36 -0500

    Estilos dinámicos con props

---
+++ 70. Estilos dinámicos con props/start-react/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Title from "./components/Title";
+import Button from "./components/button/Button";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Title />
+        <Button bgColor='red'>Click aquí</Button>
+      </>
+    );
+  }
+}
+
+export default App;
