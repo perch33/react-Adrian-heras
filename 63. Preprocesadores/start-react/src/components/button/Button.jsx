32c60cd9871f037c112519ee15f4c9c2e3a1d692
percy chuzon <percyrondan1995@gmail.com>
2023-04-22 20:28:22 -0500

    preprocesadores

---
+++ 63. Preprocesadores/start-react/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+/* import "./button.css"; */
+import "./button.scss"
+
+/* npm add -D sass "en vite"  */
+
+const Button = () => {
+  return <button className="button">Click Me!</button>;
+};
+
+export default Button;
